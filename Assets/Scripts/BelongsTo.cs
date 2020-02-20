c1ed4396e9df6fd475b9e52b451cafaf9d98eb52
Alessandro Costella <alessandrocostella@hotmail.it>
2020-02-20 14:58:12 +0100

    Aggiunti Start e Update alla palla

--- Assets/Scripts/BelongsTo.cs
+++ Assets/Scripts/BelongsTo.cs
@@ -6,10 +6,22 @@ public class BelongsTo : MonoBehaviour
 {
     //private Rigidbody rigidBody;
     public GameObject player = null;
+    private Rigidbody rb;
+
     public void SetPlayer(GameObject Player)
     {
         player = Player;
         if (Player != null) transform.position = player.transform.position + new Vector3(0, 0.9f, 0);
     }
 
+    public void Start() { rb = GetComponent<Rigidbody>(); }
+
+    public void Update()
+    {
+        if (player != null) 
+        {
+            Vector3 newPos = player.transform.position + new Vector3(0, 0.9f, 0);
+            rb.MovePosition(newPos);
+        }
+    }
 }
